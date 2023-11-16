7a1bb2fd54411005dc0ec566a33b1d04672d93e9
vickda <vdas@hawk.iit.edu>
2023-11-15 20:59:54 -0600

    storage logic to store files in Azure

--- server.js
+++ server.js
@@ -1,99 +1,122 @@
 const express = require('express');
 const multer = require('multer');
+const { BlobServiceClient } = require('@azure/storage-blob');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Set up Azure Storage Blob Service
+const connectionString = 'DefaultEndpointsProtocol=https;AccountName=filelab;AccountKey=xszf6EZ+eSpY8e2y0qF1Vmk/7ovg4OfjUTojwv1kml3EfiEv0kWhRsD0BShvxbDPEsPKBD07M/Hx+AStI09eKQ==;EndpointSuffix=core.windows.net'; // Replace with your actual connection string
+const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+const containerName = 'files'; // Replace with your actual container name
+const containerClient = blobServiceClient.getContainerClient(containerName);
 
-
-// Set storage engine for overwrite files
-const storage = multer.diskStorage({
-  destination: './uploads/',
-  filename: function (req, file, cb) {
-    const originalname = file.originalname;
-    cb(null, originalname);
-  },
-});
-
-// Set storage engine for duplicate files
-const storagedupli = multer.diskStorage({
-  destination: './uploads/',
-  filename: function (req, file, cb) {
-    const originalname = file.originalname;
-    let newFilename = originalname;
-
-    if (fs.existsSync(path.join('./uploads/', originalname))) {
-      const { name, ext } = path.parse(originalname);
-      let i = 1;
-      while (fs.existsSync(path.join('./uploads/', `${name}_duplicate_${i}${ext}`))) {
-        i++;
-      }
-      newFilename = `${name}_duplicate_${i}${ext}`;
-    }
-    cb(null, newFilename);
-  },
-});
-
-
-
-const upload = multer({ storage });
-const uploaddupli = multer({ storage: storagedupli });
+// Set storage engine for multer (file upload)
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // Routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.redirect('/');
+// Route to handle file upload
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    const file = req.file;
+    const fileName = file.originalname;
+
+    // Upload file to Azure Storage
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+    await blockBlobClient.upload(file.buffer, file.size);
+
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-app.post('/uploadduplicate', uploaddupli.single('file'), (req, res) => {
-  res.redirect('/');
+// Route to handle file upload with duplicate logic
+app.post('/uploadduplicate', upload.single('file'), async (req, res) => {
+  try {
+    const file = req.file;
+    const originalName = file.originalname;
+    let newFilename = originalName;
+
+    if (await containerClient.getBlockBlobClient(originalName).exists()) {
+      const { name, ext } = path.parse(originalName);
+      let i = 1;
+      while (await containerClient.getBlockBlobClient(`${name}_duplicate_${i}${ext}`).exists()) {
+        i++;
+      }
+      newFilename = `${name}_duplicate_${i}${ext}`;
+    }
+
+    // Upload file to Azure Storage
+    const blockBlobClient = containerClient.getBlockBlobClient(newFilename);
+    await blockBlobClient.upload(file.buffer, file.size);
+
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-
-app.get('/files', (req, res) => {
-  fs.readdir('./uploads/', (err, files) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Internal Server Error');
+// Route to get a list of files
+app.get('/files', async (req, res) => {
+  try {
+    const files = [];
+    for await (const blobItem of containerClient.listBlobsFlat()) {
+      files.push(blobItem.name);
     }
 
     res.json(files);
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-app.delete('/delete/:filename', (req, res) => {
-  const filename = req.params.filename;
-  const filePath = path.join('./uploads/', filename);
+// Route to handle file deletion
+app.delete('/delete/:filename', async (req, res) => {
+  try {
+    const filename = req.params.filename;
 
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Internal Server Error');
-    }
+    // Delete file from Azure Storage
+    const blockBlobClient = containerClient.getBlockBlobClient(filename);
+    await blockBlobClient.delete();
 
     res.sendStatus(200);
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Route for dnwloading file
-app.get('/download/:filename', (req, res) => {
-  const filename = req.params.filename;
-  const filePath = path.join('./uploads/', filename);
+// Route for downloading file
+app.get('/download/:filename', async (req, res) => {
+  try {
+    const filename = req.params.filename;
 
-  res.download(filePath, (err) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-  });
+    // Download file from Azure Storage
+    const blockBlobClient = containerClient.getBlockBlobClient(filename);
+    const downloadBlockBlobResponse = await blockBlobClient.download();
+
+    // Set response headers for file download
+    res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+    res.setHeader('Content-Type', downloadBlockBlobResponse.contentType);
+
+    // Pipe the file stream to the response
+    downloadBlockBlobResponse.readableStreamBody.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // Start server
